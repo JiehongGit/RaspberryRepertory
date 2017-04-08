54f044c20f3028122ab906bfc66777770e3dc1dc
KnightGithub <974861604@qq.com>
2017-04-08 21:27:32 +0800

    树莓派视频监控

--- camera.py
+++ camera.py
@@ -1,31 +1,31 @@
-# import the necessary packages
+# -*- coding: UTF-8 -*- 
+# 导入必要的软件包
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
 
-# initialize the camera and grab a reference to the raw camera capture
+# 初始化相机并抓取原始相机捕获的引用
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
-# allow the camera to warmup
+# 让摄像头进行热身
 time.sleep(0.1)
 
-# capture frames from the camera
+# 从摄像头捕捉帧
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
+          # 抓住原NumPy数组表示的图像，然后初始化时间戳
     image = frame.array
 
-    # show the frame
+          # 展示frame
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
 
-    # clear the stream in preparation for the next frame
+          # 在准备下一帧时清除流
     rawCapture.truncate(0)
 
-    # if the `q` key was pressed, break from the loop
+          # 按q退出frame
     if key == ord("q"):
         break
